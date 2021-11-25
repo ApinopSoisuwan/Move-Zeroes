72a431d4f9360ad2ad4c8a2d41af33102a4b584a
Apinop <apinop.soisuwan@gmail.com>
2021-11-25 16:32:55 +0700

    done but two point not load

--- Move zero.py
+++ Move zero.py
@@ -2,15 +2,20 @@ def moveZeroes(nums):
     """
     Do not return anything, modify nums in-place instead.
     """
-    c = nums.count(0)
-    for i in range(c):
-        nums.remove(0)
-        nums.append(0)
+    l = 0
+    h = len(nums)
+    for i in range(h):
+        if nums[i] != 0 :
+            nums[i],nums[l] = nums[l],nums[i]
+            l += 1
     return nums
 
-
-
-
+"""
+   if i < n:
+       A[n], A[i:n] = A[i], A[i+1:n+1]
+    else:
+       A[n], A[n+1:i+1] = A[i], A[n:i]
+"""
 a1 = [0,1,0,3,12]
 #Output: [1,3,12,0,0]
 
@@ -18,8 +23,8 @@ a2 = [0]
 #Output: [0]
 print(moveZeroes(a1))
 """
-bit load 
+light !
 result 
-Runtime: 647 ms, faster than 13.14% of Python3 online submissions for Move Zeroes.
-Memory Usage: 15.7 MB, less than 10.06% of Python3 online submissions for Move Zeroes.
+Runtime: 152 ms, faster than 99.29% of Python3 online submissions for Move Zeroes.
+Memory Usage: 15.4 MB, less than 70.30% of Python3 online submissions for Move Zeroes.
 """
