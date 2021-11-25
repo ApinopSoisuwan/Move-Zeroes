a6c7e5c1151912189b276dc64400081704c5d7cc
Apinop <apinop.soisuwan@gmail.com>
2021-11-25 16:21:26 +0700

    done but bit load

--- Move zero.py
+++ Move zero.py
@@ -3,11 +3,14 @@ def moveZeroes(nums):
     Do not return anything, modify nums in-place instead.
     """
     c = nums.count(0)
-    while 0 in nums:
-        nums.remove(0)
     for i in range(c):
+        nums.remove(0)
         nums.append(0)
     return nums
+
+
+
+
 a1 = [0,1,0,3,12]
 #Output: [1,3,12,0,0]
 
@@ -15,8 +18,8 @@ a2 = [0]
 #Output: [0]
 print(moveZeroes(a1))
 """
-heavy load
+bit load 
 result 
-Runtime: 968 ms, faster than 11.08% of Python3 online submissions for Move Zeroes.
-Memory Usage: 15.7 MB, less than 10.06% of Python3 online submissions for Move Zeroes. 
+Runtime: 647 ms, faster than 13.14% of Python3 online submissions for Move Zeroes.
+Memory Usage: 15.7 MB, less than 10.06% of Python3 online submissions for Move Zeroes.
 """
